b013711e1ec891bf54a83a2a8f72947da8d552d1
SairamKoundinya <urstrulysairam2h@gmail.com>
2019-08-08 12:59:55 +0530

    Initial commit

---
+++ app/src/main/java/arscope/android/com/gpapracapp/MainActivity.java
@@ -0,0 +1,29 @@
+package arscope.android.com.gpapracapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.result);
+
+
+    }
+    public void save(EditText v){
+
+        int number=Integer.parseInt(v.getText().toString());
+
+
+
+    }
+}
